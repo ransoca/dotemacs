8464dc4a41751a04609a78a97b5d03144fc0d3ed
Rans <mail@rans.page>
2025-03-21 13:32:27 +0200

    chore: updated or patched

--- personal/preload/custom-config.el
+++
@@ -1,3 +0,0 @@
-(setq prelude-theme 'solarized-dark)
-(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-14"))
-(set-face-attribute 'default t :font "DejaVu Sans Mono-14")
--- personal/preload/go-mode-config.el
+++
@@ -1,4 +0,0 @@
-(add-hook 'go-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook 'gofmt-before-save)
-            (setq tab-width 2)))
